5a8a165dc37abac854e3a0be946f80d40405138e
Karim <ka2566@columbia.edu>
2019-12-11 19:02:58 -0500

    Split off network related files to connectivity.py.

--- myst_sw/main.py
+++ myst_sw/main.py
@@ -7,43 +7,13 @@
 # Written by Karim Abdallah on Saturday,  7 December 2019.
 #/
 
-import network
 from time import sleep
 
 from initialize import *
 from pump import *
-
-networkSSID = 'iRobot-Guest'
-networkPsswd = ''
-
-networkConTimeout = 10
-networkTmrInc = 0.1
+from connectivity import *
 
 squirtDuration = 5 # How long should the pump squirt for
-
-class NetworkError(BaseException):
-    pass
-
-def configureNetwork ():
-    
-    wlan = network.WLAN(network.STA_IF)
-    networkTimer = 0;
-    
-    wlan.active(True)
-    
-    if not wlan.isconnected():
-        print("connecting to network", networkSSID, networkPsswd)
-        wlan.connect(networkSSID, networkPsswd)
-
-    while (not wlan.isconnected() and networkTimer < networkConTimeout):
-        sleep(networkTmrInc)
-        networkTimer += networkTmrInc
-        
-    if not wlan.isconnected():
-        print("Couldn't connect to the network")
-    else:
-        print("Network Succesfully Configured.")
-        print("network config:", wlan.ifconfig())
 
 
 def main():
